4cccc29e5632fdb828647eae8d333ed76421976b
Rodrigo Graça <rodrigograca31@gmail.com>
2020-10-12 19:24:27 +0100

    feat: 🎸 Bob

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,7 +1,38 @@
 class Bob {
-    hey( /* Parameters go here */ ) {
-        // Your code here
+  hey(phrase: string): string {
+    phrase = phrase.trim();
+
+    const hasLetters = phrase.match(/[a-z]/i);
+
+    if (phrase === "") {
+      return "Fine. Be that way!";
+    } else if (
+      phrase.toUpperCase() === phrase &&
+      phrase.endsWith("?") &&
+      hasLetters
+    ) {
+      return "Calm down, I know what I'm doing!";
+    } else if (phrase.endsWith("?")) {
+      return "Sure.";
+    } else if (phrase.toUpperCase() === phrase && hasLetters) {
+      return "Whoa, chill out!";
     }
+
+    return "Whatever.";
+
+    // switch (type) {
+    //   case "?":
+    //     return "Sure.";
+    //   case "!":
+    //     return "Whoa, chill out!";
+    //   case "":
+    //     return "Fine. Be that way!";
+    //   case "wtf":
+    //     return "Calm down, I know what I'm doing!";
+    //   default:
+    //     return "Whatever.";
+    // }
+  }
 }
 
-export default Bob
+export default Bob;
